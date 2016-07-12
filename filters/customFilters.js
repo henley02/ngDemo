5355320f316addaa900f9baa7b2c81dfb321ae99
henley02 <410451447@qq.com>
2016-07-12 14:03:08 +0800

    commit

--- filters/customFilters.js
+++ filters/customFilters.js
@@ -54,3 +54,72 @@ angular.module("sportsStore")
             }
         }
     })
+    //获取url参数
+    .factory('getParamsCode', [function () {
+        return function (name) {
+            var search = window.location.href.indexOf('?') > -1 ? window.location.href.split('?')[1].split("&") : [],
+                i = 0, len = search.length,
+                params = {}, pos;
+
+            for (; i < len; i++) {
+                pos = search[i].indexOf("=");
+                if (pos > 0) {
+                    params[search[i].substring(0, pos)] = decodeURIComponent(search[i].substring(pos + 1));
+                }
+            }
+            return params[name] ? params[name] : undefined;
+        }
+    }])
+//获取浏览器类型
+    .service('browser', [function () {
+        var u = navigator.userAgent.toLowerCase();
+        return {
+            txt: u, // 浏览器版本信息
+            version: (u.match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/) || [])[1], // 版本号
+            msie: /msie/.test(u) && !/opera/.test(u), // IE内核
+            mozilla: /mozilla/.test(u) && !/(compatible|webkit)/.test(u), // 火狐浏览器
+            safari: /safari/.test(u) && !/chrome/.test(u), //是否为safair
+            chrome: /chrome/.test(u), //是否为chrome
+            opera: /opera/.test(u), //是否为oprea
+            presto: u.indexOf('presto/') > -1, //opera内核
+            webKit: u.indexOf('applewebkit/') > -1, //苹果、谷歌内核
+            gecko: u.indexOf('gecko/') > -1 && u.indexOf('khtml') == -1, //火狐内核
+            mobile: !!u.match(/applewebkit.*mobile.*/), //是否为移动终端
+            ios: !!u.match(/\(i[^;]+;( u;)? cpu.+mac os x/), //ios终端
+            android: u.indexOf('android') > -1, //android终端
+            iPhone: u.indexOf('iphone') > -1, //是否为iPhone
+            iPad: u.indexOf('ipad') > -1, //是否iPad
+            weixin: /micromessenger/.test(u), //微信
+            QQBrowse: u.indexOf(' QQ') > -1 || u.indexOf(' qq') > -1,
+            webApp: !!u.match(/applewebkit.*mobile.*/) && u.indexOf('safari/') == -1 //是否web应该程序，没有头部与底部
+        };
+    }])
+    .directive('onLastRepeat', function ($timeout) {
+        return function (scope, element, attrs) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit('ngRepeatFinished');
+                }, 100);
+            }
+        };
+    })
+    //下拉加载
+    .factory('dropDown', [function () {
+        return function (callback) {
+
+            window.onscroll = function () {
+
+                var Allheight = document.body.scrollHeight,
+                    windowHeight = window.innerHeight,
+                    scroolTop = document.body.scrollTop;
+
+                if ((scroolTop + 10) >= Allheight - windowHeight) {
+
+                    if (callback)
+                        callback();
+                }
+
+            };
+
+        };
+    }])
